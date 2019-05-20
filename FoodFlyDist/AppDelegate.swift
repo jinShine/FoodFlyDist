19e9b1dc68191da8a02c7b29530770187e4a157d
seungjin <seungjin429@foodfly.co.kr>
2019-05-20 11:22:48 +0900

    git init

---
+++ FoodFlyDist/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FoodFlyDist
+//
+//  Created by Seungjin on 20/05/2019.
+//  Copyright © 2019 승진김. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FoodFlyDist/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FoodFlyDist
+//
+//  Created by Seungjin on 20/05/2019.
+//  Copyright © 2019 승진김. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
